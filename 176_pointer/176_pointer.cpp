444b869aa314f7cdbf7b523164e1b3a02054b5f7
riskalaluna <riskalaluna@gmail.com>
2023-05-13 12:20:18 +0700

    5

--- 176_pointer/176_pointer.cpp
+++ 176_pointer/176_pointer.cpp
@@ -16,4 +16,12 @@ int main()
     mahasiswa mhs{ 1 };         //object mhs
     mhs.showNim();              //Member access Operator
 
-  
+    mahasiswa& ref = mhs;       // Pinter Reference refMhs
+    ref.nim = 2;                // Member acces Operator
+    mhs.showNim();
+
+    mahasiswa* pMhs = &mhs;     // Pointer deference pMhs
+    pMhs->nim = 3;              // Arrow Operator
+    mhs.showNim();
+    return 0;
+}
