ab97ddee6c0468568b2ce08f75a80306befaf627
riskalaluna <riskalaluna@gmail.com>
2023-05-13 12:19:24 +0700

    4

--- 176_pointer/176_pointer.cpp
+++ 176_pointer/176_pointer.cpp
@@ -11,3 +11,9 @@ public:
 void mahasiswa::showNim() {     //implementasi method diluar class
     cout << "No Induk = " << nim << endl;
 }
+int main()
+{
+    mahasiswa mhs{ 1 };         //object mhs
+    mhs.showNim();              //Member access Operator
+
+  
