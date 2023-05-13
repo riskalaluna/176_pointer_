9042a3a9543628033ae042f6055b255ee374aca1
riskalaluna <riskalaluna@gmail.com>
2023-05-13 12:17:32 +0700

    3

--- 176_pointer/176_pointer.cpp
+++ 176_pointer/176_pointer.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+    int nim;
+    void showNim();     //deklarasi method
+};
+
+
+void mahasiswa::showNim() {     //implementasi method diluar class
+    cout << "No Induk = " << nim << endl;
+}
